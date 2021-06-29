0db33493d3940122b695db9ef431a60fac8527df
dana-jpg <85879403+dana-jpg@users.noreply.github.com>
2021-06-29 12:26:30 +0200

    update2

--- Assets/Scripts/UserMovement.cs
+++ Assets/Scripts/UserMovement.cs
@@ -12,8 +12,7 @@ public class UserMovement : MonoBehaviour
     public float rotateSpeed;
     public float scrollSpeed;
     public GameObject playerCamera;
-    public int count;
-    public TMP_Text countText;
+    
 
    
 
@@ -30,12 +29,12 @@ public class UserMovement : MonoBehaviour
     //Texts
     public GameObject TransportEfficiencyText;
     public GameObject InstructionText;
-    //public GameObject EnergySupplyText;
-    //public GameObject EmissionsText;
-    //public GameObject CarbonRemovalText;
-    //public GameObject GrowthText;
-    //public GameObject BuildingsIndustryText;
-    //public GameObject PlantText;
+    public GameObject EnergySupplyText;
+    public GameObject EmissionsText;
+    public GameObject CarbonRemovalText;
+    public GameObject GrowthText;
+    public GameObject BuildingsIndustryText;
+    public GameObject PlantText;
 
     //Minigems
     public GameObject TransportEfficiencyGemMini;
@@ -48,17 +47,28 @@ public class UserMovement : MonoBehaviour
     public GameObject PlantGemMini;
 
 
-    public GameObject FoundGemText;
+    //count and inventory
+    public int count;
+    public TMP_Text countText;
+    
+    public GameObject ChallengeBackground;
+    public Material[] materials;
+    public TMP_Text challengeText;
+
     public GameObject InventoryText;
     public GameObject ChallengeText;
+
+    //random
+    public GameObject FoundGemText;
     public GameObject TestText;
-    
 
     void Start()
     {
         Debug.Log("start done");
         count = 0;
         SetCountText();
+        SetChallengeProgress();
+        SetChallengeText();
 
 
         //deactivate gems
@@ -90,11 +100,18 @@ public class UserMovement : MonoBehaviour
 
         TransportEfficiencyText.SetActive(false);
         InstructionText.SetActive(false);
-        
+        EnergySupplyText.SetActive(false);
+        EmissionsText.SetActive(false);
+        CarbonRemovalText.SetActive(false);
+        GrowthText.SetActive(false);
+        BuildingsIndustryText.SetActive(false);
+        PlantText.SetActive(false);
+
+
     }
 
-// Update is called once per frame
-void Update()
+    // Update is called once per frame
+    void Update()
     {
         if (Input.GetKey(KeyCode.LeftShift) && Input.GetKey(KeyCode.UpArrow))
         {
@@ -143,19 +160,68 @@ void Update()
             TransportEfficiencyGemMini.SetActive(true);
             TransportEfficiencyText.SetActive(true);
             count++;
-            //FoundGemText.SetActive(true);
-            
+            //FoundGemText.SetActive(true);   
         }
         else if (other.gameObject.CompareTag("Instruction"))
         {
             InstructionGem.SetActive(false);
             InstructionGemMini.SetActive(true);
             InstructionText.SetActive(true);
-            count ++;
-           
+            count ++;   
         }
 
+        else if (other.gameObject.CompareTag("EnergySupply"))
+        {
+            EnergySupplyGem.SetActive(false);
+            EnergySupplyGemMini.SetActive(true);
+            EnergySupplyText.SetActive(true);
+            count++;
+        }
+
+        else if (other.gameObject.CompareTag("Emissions"))
+        {
+            EmissionsGem.SetActive(false);
+            EmissionsGemMini.SetActive(true);
+            EmissionsText.SetActive(true);
+            count++;
+        }
+
+        else if (other.gameObject.CompareTag("CarbonRemoval"))
+        {
+            CarbonRemovalGem.SetActive(false);
+            CarbonRemovalGemMini.SetActive(true);
+            CarbonRemovalText.SetActive(true);
+            count++;
+        }
+
+        else if (other.gameObject.CompareTag("Growth"))
+        {
+            GrowthGem.SetActive(false);
+            GrowthGemMini.SetActive(true);
+            GrowthText.SetActive(true);
+            count++;
+        }
+
+        else if (other.gameObject.CompareTag("BuildingsIndustry"))
+        {
+            BuildingsIndustryGem.SetActive(false);
+            BuildingsIndustryGemMini.SetActive(true);
+            BuildingsIndustryText.SetActive(true);
+            count++;
+        }
+
+        else if (other.gameObject.CompareTag("Plant"))
+        {
+            PlantGem.SetActive(false);
+            PlantGemMini.SetActive(true);
+            PlantText.SetActive(true);
+            count++;
+        }
+
+
         SetCountText();
+        SetChallengeProgress();
+        SetChallengeText();
 
     }
 
@@ -165,11 +231,36 @@ void Update()
         {
             countText.text = "\n currently, you own " + count.ToString() + " gem";
         }
+        else if (count == 8)
+        {
+            countText.text = "\n you found all of the gems!";
+        }
         else
         {
             countText.text = "\n currently, you own " + count.ToString() + " gems";
         }
-        
+
+    }
+
+    void SetChallengeProgress()
+    {    
+            ChallengeBackground.GetComponent<MeshRenderer>().material = materials[count];    
+    }
+
+    void SetChallengeText()
+    {
+        if (count == 0)
+        {
+            challengeText.text = "\n look for gems to safe the world!";
+        }
+        else if (count == 8)
+        {
+            challengeText.text = "\n challenge completed!";
+        }
+        else
+        {
+            challengeText.text = "\n find more gems to complete the challenge";
+        }
 
     }
 }
